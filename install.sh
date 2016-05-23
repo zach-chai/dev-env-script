f6ce2f2644880dfe8bdb367bd3bf83fd69466860
Zachary Chai <zachary.chai@outlook.com>
2016-05-23 11:48:02 -0400

    docker and compose

--- install.sh
+++ install.sh
@@ -10,10 +10,9 @@ fi
 apt-get update
 apt-get install -y ssh git vim curl wget zsh software-properties-common
 
-
+# Hub
 hub_version="$(wget -qO- https://api.github.com/repos/github/hub/releases/latest | grep tag_name | cut -d'"' -f4 | cut -c 2-)"
 
-# Install hub
 curl -sSL https://github.com/github/hub/releases/download/v${hub_version}/hub-linux-amd64-${hub_version}.tgz -o hub.tgz \
   && tar -xf hub.tgz \
   && ./hub-linux-amd64-${hub_version}/install \
@@ -21,7 +20,7 @@ curl -sSL https://github.com/github/hub/releases/download/v${hub_version}/hub-li
   && rm -rf hub-linux-amd64-${hub_version}
 
 
-# Install atom
+# Atom
 add-apt-repository -y ppa:webupd8team/atom
 apt-get update
 apt-get install -y atom
@@ -37,6 +36,22 @@ if [ ! -d ~/.dotfiles ]; then
   git clone https://github.com/zach-chai/dotfiles.git ~/.dotfiles
   cd ~/.dotfiles && ./install.sh
   cd ~
+fi
+
+# Docker
+if ! type "docker" > /dev/null; then
+  echo "Installing Docker..."
+  wget -qO- https://get.docker.com/ | sh
+  usermod -a -G docker $(whoami)
+fi
+
+# Docker compose
+if ! type "docker-compose" > /dev/null; then
+	echo "Installing Docker Compose..."
+  LATEST="$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d'"' -f4)"
+  wget -q https://github.com/docker/compose/releases/download/${LATEST}/docker-compose-`uname -s`-`uname -m`
+  mv docker-compose-`uname -s`-`uname -m` /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
 fi
 
 
