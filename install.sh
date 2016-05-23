4293e82474d2e14d72d58d9cdd04923c65ee14de
Zachary Chai <zachary.chai@outlook.com>
2016-05-23 11:23:19 -0400

    add hub install

--- install.sh
+++ install.sh
@@ -11,6 +11,15 @@ apt-get update
 apt-get install -y ssh git vim curl wget zsh software-properties-common
 
 
+hub_version="$(wget -qO- https://api.github.com/repos/github/hub/releases/latest | grep tag_name | cut -d'"' -f4 | cut -c 2-)"
+
+# Install hub
+curl -sSL https://github.com/github/hub/releases/download/v${hub_version}/hub-linux-amd64-${hub_version}.tgz -o hub.tgz \
+  && tar -xf hub.tgz \
+  && ./hub-linux-amd64-${hub_version}/install \
+  && rm -f hub.tgz \
+  && rm -rf hub-linux-amd64-${hub_version}
+
 # Setup dotfiles
 if [ ! -d ~/.dotfiles ]; then
   git clone https://github.com/zach-chai/dotfiles.git ~/.dotfiles
